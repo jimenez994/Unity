cf9d7226da2e0f8d92adf44badc8cd7e0ebbc843
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-02-26 14:33:10 -0500

    _

--- LearnToCode/Assets/Conditionals.cs
+++ LearnToCode/Assets/Conditionals.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Conditionals : MonoBehaviour {
+	int health = 100;
+	bool gameOver = false;
 
 	// Use this for initialization
 	void Start () {
-		
+		if (health <= 0) {
+			gameOver = true;
+		}
+
+		if(gameOver == true ){
+			Debug.Log ("The game is Over!");
+		}else{
+			Debug.Log ("You're still alive buddy!");
+		}
+
+
 	}
 	
 	// Update is called once per frame
